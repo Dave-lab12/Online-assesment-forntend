58cc5130aa8e3d0d9608540087df4e91b361a110
Dave <Dwtmekonnen123@gmail.com>
2022-07-05 09:45:00 +0300

    true false UI complited added dependency for confetti

--- components/TrueFalse.jsx
+++ components/TrueFalse.jsx
@@ -2,33 +2,64 @@ import React, { useState } from "react";
 import axios from "axios";
 import { BASE_URL } from "../Api";
 import { Radio, Button, notification } from "antd";
+import styles from "../styles/questions.module.css";
+
 const TrueFalse = ({ userId, setQuestionsCounter, questionId }) => {
   const [answer, setAnswer] = useState({});
-
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
   const handleSubmit = async () => {
     const sendAnswer = await axios.post(`${BASE_URL}/answers`, {
       data: { Answer: answer, intern: userId, question: questionId },
     });
-
     if (sendAnswer.status === 200) {
       setQuestionsCounter((questionsCounter) => questionsCounter + 1);
+      console.log(error);
+      setError(true);
+      setLoading(false);
     }
+  };
+  const openNotification = (placement, type, content) => {
+    notification[type]({
+      message: `warning`,
+      description: content,
+      placement,
+    });
   };
   const handleChange = (e) => {
     setAnswer(e.target.value);
   };
+  if (error) {
+    openNotification(
+      "top",
+      "error",
+      "Something Went Wrong Please contact support"
+    );
+  }
   return (
-    <div>
-      <Radio.Group name="choice">
-        <Radio value="false" onChange={(e) => handleChange(e)}>
+    <div className={styles.trueFalseCntainer}>
+      <Radio.Group
+        name="choice"
+        style={{ display: "flex", flexDirection: "column" }}
+      >
+        <Radio
+          value="false"
+          onChange={(e) => handleChange(e)}
+          style={{ margin: "20px 0" }}
+        >
           False
         </Radio>
-        <Radio value="true" onChange={(e) => handleChange(e)}>
+        <Radio
+          value="true"
+          onChange={(e) => handleChange(e)}
+          style={{ margin: "20px 0" }}
+        >
           true
         </Radio>
       </Radio.Group>
 
       <Button
+        style={{ display: "flex", margin: "auto" }}
         type="primary"
         loading={loading}
         size={"large"}
--- pages/completed.jsx
+++ pages/completed.jsx
@@ -1,7 +1,20 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import Confetti from "react-confetti";
 const Completed = () => {
-  return <div>Completed</div>;
+  const [height, setHeight] = useState(0);
+  const [width, setWidth] = useState(0);
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      console.log(setHeight(window.innerHeight), setWidth(window.innerWidth));
+    });
+    console.log("wtf");
+  }, []);
+  // const { width, height } = useWindowSize();
+  return (
+    <div>
+      <Confetti width={width} height={height} />
+    </div>
+  );
 };
 
 export default Completed;
