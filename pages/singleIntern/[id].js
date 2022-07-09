c3d7b0c29208bd742f5a52c4f318b6295dd40c24
Dave <Dwtmekonnen123@gmail.com>
2022-07-09 18:49:49 +0300

    UI send request after it got the id from the url instead of sending undefined

--- pages/singleIntern/[id].js
+++ pages/singleIntern/[id].js
@@ -6,15 +6,26 @@ const SingleIntern = () => {
     const [singleIntern, setSingleIntern] = useState({})
     const router = useRouter()
     const { id } = router.query
+    useEffect(() => {
+        if (id) {
+            getSingleIntern()
+        }
+    }, [id])
     const getSingleIntern = async () => {
-        const res = await axios.get(`${BASE_URL}/interns/${id}`)
+
+        const res = await axios.get(`${BASE_URL}/interns/${id}?populate[answers][populate]=question
+        `)
         setSingleIntern(res.data)
     }
-    useEffect(() => {
-        getSingleIntern()
-    }, [])
+
     console.log(singleIntern);
     return <p>SingleIntern: {id}</p>
 }
 
 export default SingleIntern
+//url to get interns id answer and quesrion
+//http://localhost:1337/api/interns/156?populate[answers][populate]=question
+
+//url to get Question-answers with question
+
+//http://localhost:1337/api/question-answers?populate=*
