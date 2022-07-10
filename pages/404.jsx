10f689b73f99e8bd8007a31374fa68b5be467f57
Dave <Dwtmekonnen123@gmail.com>
2022-07-10 17:00:56 +0300

    button redirects to the homepage

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Router from "next/router";
+
 import { Button, Result } from "antd";
 const PageNotFound = () => {
   return (
@@ -6,7 +8,11 @@ const PageNotFound = () => {
       status="404"
       title="404"
       subTitle="Sorry, the page you visited does not exist."
-      extra={<Button type="primary">Back Home</Button>}
+      extra={
+        <Button type="primary" onClick={() => Router.push("/")}>
+          Back Home
+        </Button>
+      }
     />
   );
 };
