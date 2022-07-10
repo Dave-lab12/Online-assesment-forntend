f55672b1f0c9db3b8ac5b3a273e8e7584fbafac8
Dave <Dwtmekonnen123@gmail.com>
2022-07-10 14:49:19 +0300

    data table for each intern to see the answers and correct answer so admin can have easier time filtering them

--- pages/singleIntern/[id].js
+++ pages/singleIntern/[id].js
@@ -5,21 +5,82 @@ import { useState, useEffect } from 'react'
 import { Table } from 'antd'
 const SingleIntern = () => {
     const [singleIntern, setSingleIntern] = useState({})
+    const [correctAns, setCorrectAns] = useState(null)
+    const answerLength = singleIntern?.data?.attributes?.answers?.data?.length
     const router = useRouter()
     const { id } = router.query
+    const columns = [
+        {
+            title: 'question ID',
+            dataIndex: 'questionId',
+        },
+        {
+            title: 'question title',
+            dataIndex: 'question',
+            width: "30%",
+        },
+        {
+            title: 'Answer',
+            dataIndex: 'answer',
+        },
+        {
+            title: 'correct answer',
+            dataIndex: 'correctAnswer',
+        },
+        {
+            title: 'correct',
+            dataIndex: 'correct',
+        },
+    ];
+    const data = [];
+    const filterCorrectAns = (questionID) => {
+        return correctAns && correctAns.map((item, index) => {
+            if (item.id === questionID) {
+                return item.attributes.question_answer.data.attributes.answer
+            }
+        })
+    }
+    singleIntern.data && singleIntern.data.attributes.answers.data.map((item, index) => {
+        const questionId = item.attributes.question.data.id
+        const question = item.attributes.question.data.attributes.Title
+        const answer = item.attributes.Answer
+        const correctAnswer = filterCorrectAns(questionId)
+        console.log(filterCorrectAns(questionId));
+        const correct = answer === correctAnswer ? 'true' : 'false'
+        data.push({
+            key: index,
+            questionId,
+            question,
+            answer,
+            correctAnswer,
+            correct,
+        })
+    })
+
+
     useEffect(() => {
         if (id) {
             getSingleIntern()
+            getCorrectAns()
         }
     }, [id])
+    const getCorrectAns = async () => {
+        const correctAns = await axios.get(`${BASE_URL}/questions?populate=question_answer`)
+        setCorrectAns(correctAns.data.data)
+    }
+
+
     const getSingleIntern = async () => {
         const res = await axios.get(`${BASE_URL}/interns/${id}?populate[answers][populate]=question
         `)
         setSingleIntern(res.data)
     }
 
-    console.log(singleIntern);
-    return <p>SingleIntern: {id}</p>
+    // console.log(singleIntern);
+    return <>
+        <p>SingleIntern: {id}</p>
+        <Table columns={columns} dataSource={data} />
+    </>
 }
 
 export default SingleIntern
@@ -27,5 +88,5 @@ export default SingleIntern
 //http://localhost:1337/api/interns/156?populate[answers][populate]=question
 
 //url to get Question-answers with question
-
+//http://localhost:1337/api/questions?populate=question_answer
 //http://localhost:1337/api/question-answers?populate=*
