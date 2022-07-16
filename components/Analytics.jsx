4188665af2b37055e154455369ef428dc118b768
Dave <Dwtmekonnen123@gmail.com>
2022-07-16 15:56:32 +0300

    analytics component and pie chart to visulaize interns os and browser

---
+++ components/Analytics.jsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import InternGraph from "./internGraph";
+
+const Analytics = ({ internAnalytics }) => {
+  const [osType, setOsType] = useState([]);
+  const [browser, setBrowser] = useState([]);
+  const osData = [];
+  const browserData = [];
+  function countDuplicates(arr) {
+    const osType = {};
+    arr.forEach(function (value) {
+      osType[value] = 0;
+    });
+    arr.forEach(function (value) {
+      osType[value]++;
+    });
+    return osType;
+  }
+
+  useEffect(() => {
+    if (internAnalytics.data) {
+      internAnalytics.data.map((intern) => {
+        osData.push(intern.attributes.osType);
+        browserData.push(intern.attributes.browser);
+      });
+      setOsType(countDuplicates(osData));
+      setBrowser(countDuplicates(browserData));
+    }
+  }, [internAnalytics]);
+
+  const osKeys = Object.keys(osType);
+  const browserKeys = Object.keys(browser);
+  osKeys &&
+    osKeys.forEach((item) => {
+      osData.push({
+        type: item,
+        value: osType[item],
+      });
+    });
+  browserKeys &&
+    browserKeys.forEach((item) => {
+      browserData.push({
+        type: item,
+        value: browser[item],
+      });
+    });
+
+  return (
+    <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <div style={{ flex: 1 }}>
+        <h1>Operating Systems Used</h1>
+        <InternGraph data={osData} />;
+      </div>
+      <div style={{ flex: 1 }}>
+        <h1>Browsers used</h1>
+        <InternGraph data={browserData} />;
+      </div>
+    </div>
+  );
+};
+
+export default Analytics;
---
+++ components/InternGraph.jsx
@@ -0,0 +1,25 @@
+import { Pie } from "@ant-design/plots";
+const InternGraph = ({ data }) => {
+  const config = {
+    appendPadding: 10,
+    data,
+    angleField: "value",
+    colorField: "type",
+    radius: 0.8,
+    label: {
+      type: "outer",
+      content: "{name} {percentage}",
+    },
+    interactions: [
+      {
+        type: "pie-legend-active",
+      },
+      {
+        type: "element-active",
+      },
+    ],
+  };
+  return <Pie {...config} />;
+};
+
+export default InternGraph;
